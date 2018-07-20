5bab54d7851d1b17a73d7badabedc06721a7e1bf
Mai Campbell <jirallc@gmail.com>
2018-07-20 16:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function nowServing(i) {
   }
 }
 
-function currentLine(l) {
-  var line = [];
-  if (l.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    for(let i = 0; i < l.length; i++) {
-      line += (i + 1) + "." + l[i] + ","
+function currentLine(x) {
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + x[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
     }
-    line = line.splice(0, line.length - 2);
-    return "The line is currently " + line;
-  }
 }
